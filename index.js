e7fabea4e74c7866901b16d87a7c93a7089b2099
yfern328 <yfernan2@u.rochester.edu>
2017-07-18 17:59:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // Add your doToElementsInArray() function here:
 
 
+function doToElementsInArray(array, callback) {
+  array.forEach(callback);
+}
+
 // Add your changeCompletely() function here:
+
+function changeCompletely(element, index, array) {
+  array[index] = "<3";
+}
